5f8ab0eaf7bb925c8d7de11d196f7ac8444f94b3
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-02-07 13:56:02 +0900

    firebase　接続

--- firebase-auth-test/src/main.ts
+++ firebase-auth-test/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
+  const adminConfig: ServiceAccount = {
+    projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
+    privateKey: configService
+      .get<string>('FIREBASE_PRIVATE_KEY')
+      .replace(/\\n/g, '\n'),
+    clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+  };
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+  });
+
   await app.listen(3000);
 }
 bootstrap();
