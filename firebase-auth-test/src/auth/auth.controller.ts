7b6dc6a1dea4367c5ec466a1c007f840cc931f9f
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-02-08 12:41:56 +0900

    signup設定

--- firebase-auth-test/src/auth/auth.controller.ts
+++ firebase-auth-test/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+import { SignUpModel } from './models/signup.model';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +11,15 @@ export class AuthController {
       console.log(user.toJSON());
     });
     return users;
+  }
+
+  @Post('signup')
+  async signUp(@Body() signupModel: SignUpModel) {
+    console.log('signupModel', signupModel);
+    const user = await admin.auth().createUser({
+      email: signupModel.email,
+      password: signupModel.password,
+    });
+    return user;
   }
 }
