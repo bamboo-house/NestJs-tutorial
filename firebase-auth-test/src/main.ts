19142d3571f1f4445223e2b92cbd903f54aae04e
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-02-09 00:17:58 +0900

    とりあえず確認おk

--- firebase-auth-test/src/main.ts
+++ firebase-auth-test/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
 import { ConfigService } from '@nestjs/config';
+import { initializeApp } from 'firebase/app';
+import { FirebaseApp } from '@firebase/app';
+
+export const firebaseApp: FirebaseApp = undefined;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +18,11 @@ async function bootstrap() {
       .replace(/\\n/g, '\n'),
     clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
   };
+
+  const firebaseConfig = {
+    apiKey: configService.get<string>('FIREBASE_API_KEY'),
+  };
+
   admin.initializeApp({
     credential: admin.credential.cert(adminConfig),
   });
